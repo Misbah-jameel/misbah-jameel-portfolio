fdcaca98f988df419532e804000b9cbb7eda7a68
Misbah Jameel <162004635+Misbah-jameel@users.noreply.github.com>
2025-02-12 19:11:21 +0500

    Update page.tsx

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,23 +1,21 @@
 
 import Hero from '@/app/Hero/Hero'
-import Navbar from '../components/Navbar';
 import About from '../components/About';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Education from '../components/Education';
 import Contact from '../components/Contact';
-import Footer from '../components/Footer';
+
 export default function Home() {
   return (
     <div>
-      <Navbar />
+
       <Hero/>
       <About />
       <Skills />
       <Projects />
       <Education />
       <Contact />
-      <Footer />
     </div>
   );
 }
